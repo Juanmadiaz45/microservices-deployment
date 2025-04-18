0022438a76579dc9bd08fbdfcdeb62d99fdde8ca
Juan Manuel <Juanmadiaz45>
2025-04-18 18:27:15 -0500

    feat: ansible and terraform implementation for the microservices deployment

---
+++ terraform/modules/microservices-vm/variables.tf
@@ -0,0 +1,28 @@
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "subnet_id" {
+  type        = string
+}
+
+variable "vm_name" {
+  type        = string
+}
+
+variable "vm_size" {
+  type        = string
+}
+
+variable "admin_username" {
+  type        = string
+}
+
+variable "admin_password" {
+  type        = string
+  sensitive   = true
+}
