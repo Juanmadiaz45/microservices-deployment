b32d2b635b372e069b9ba91302bb979f87d73780
Juan Manuel <Juanmadiaz45>
2025-04-22 01:07:10 -0500

    feat: circuit breaker changes

--- terraform/modules/function-app/main.tf
+++ terraform/modules/function-app/main.tf
@@ -11,7 +11,33 @@ resource "azurerm_service_plan" "function_plan" {
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Windows"
-  sku_name            = "Y1" # Consumption plan
+  sku_name            = "Y1"
+}
+
+resource "null_resource" "prepare_function_app" {
+  triggers = {
+    index_js_hash = filesha256("${path.module}/circuit-breaker/CircuitBreaker/index.js"),
+    function_json_hash = filesha256("${path.module}/circuit-breaker/CircuitBreaker/function.json"),
+    package_json_hash = filesha256("${path.module}/circuit-breaker/package.json"),
+    host_json_hash = filesha256("${path.module}/circuit-breaker/host.json")
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      rm -rf ${path.module}/deploy
+      mkdir -p ${path.module}/deploy
+      cp -r ${path.module}/circuit-breaker/* ${path.module}/deploy/
+      cd ${path.module}/deploy
+      npm install --production
+    EOT
+  }
+}
+
+data "archive_file" "function_app_package" {
+  type        = "zip"
+  source_dir  = "${path.module}/deploy"
+  output_path = "${path.module}/function-app.zip"
+  depends_on  = [null_resource.prepare_function_app]
 }
 
 resource "azurerm_windows_function_app" "circuit_breaker" {
@@ -24,22 +50,29 @@ resource "azurerm_windows_function_app" "circuit_breaker" {
   
   site_config {
     application_stack {
-      node_version = "~18"
+      node_version = "~20"
     }
     cors {
       allowed_origins = ["*"]
     }
+    ftps_state = "Disabled"
+  }
+  
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE"       = "1",
+    "FUNCTIONS_WORKER_RUNTIME"       = "node",
+    "MICROSERVICES_VM_IP"            = var.microservices_vm_ip,
+    "AUTH_API_PORT"                  = "8000",
+    "USERS_API_PORT"                 = "8083",
+    "TODOS_API_PORT"                 = "8082",
+    "FRONTEND_PORT"                  = "8080",
+    "CIRCUIT_BREAKER_THRESHOLD"      = "3",
+    "CIRCUIT_BREAKER_TIMEOUT_MS"     = "10000",
+    "AzureWebJobsStorage"            = azurerm_storage_account.function_storage.primary_connection_string,
+    "FUNCTIONS_EXTENSION_VERSION"    = "~4",
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true",
+    "WEBSITE_NODE_DEFAULT_VERSION"   = "~20"
   }
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE"    = "1",
-    "FUNCTIONS_WORKER_RUNTIME"    = "node",
-    "MICROSERVICES_VM_IP"         = var.microservices_vm_ip,
-    "AUTH_API_PORT"               = "8000",
-    "USERS_API_PORT"              = "8083",
-    "TODOS_API_PORT"              = "8082",
-    "FRONTEND_PORT"               = "8080",
-    "CIRCUIT_BREAKER_THRESHOLD"   = "3",
-    "CIRCUIT_BREAKER_TIMEOUT_MS"  = "10000"
-  }
+  zip_deploy_file = data.archive_file.function_app_package.output_path
 }
