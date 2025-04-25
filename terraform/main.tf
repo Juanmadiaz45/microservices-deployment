df08c406b63698972aa1cf9d4903b468930b088f
MGMaikeru <13miguegonza2005@gmail.com>
2025-04-25 09:07:43 -0500

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,6 @@ provider "azurerm" {
   use_cli = true
   # Skip provider registration to avoid permission issues
   skip_provider_registration = true
-  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
